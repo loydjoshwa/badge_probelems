6f07b0ef8ea128a0c0df20f5b1ffebb08dbf340c
loy <loydjoshwad@gmail.com>
2026-06-07 23:56:30 +0530

    update

--- main.go
+++ main.go
@@ -1,125 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
-type Node struct {
-	Data int
-	Next *Node
+func dog(dogch,catch chan string, wg *sync.WaitGroup){
+	defer wg.Done()
+
+	for i:=0;i<=10;i+=2{
+		<-dogch
+		time.Sleep(time.Second)
+		fmt.Println("dog")
+		catch<-"cat"
+	}
 }
 
-// Insert at Head
-func insertHead(head *Node, data int) *Node {
-	newNode := &Node{Data: data}
+func cat(dogch,catch chan string, wg *sync.WaitGroup){
+	defer wg.Done()
 
-	newNode.Next = head
+	for i:=1;i<=9;i+=2{
+		<-catch
+		time.Sleep(time.Second)
+          fmt.Println("cat")
 
-	return newNode
+		  dogch<-"dog"
+	}
 }
 
-// Insert at Tail
-func insertTail(head *Node, data int) *Node {
-	newNode := &Node{Data: data}
+func reverse( str string) string{
 
-	if head == nil {
-		return newNode
+	if len(str)<=1 {
+		return str
 	}
 
-	temp := head
+  return reverse(str[1:])+string(str[0])
 
-	for temp.Next != nil {
-		temp = temp.Next
-	}
-
-	temp.Next = newNode
-
-	return head
 }
 
-// Insert After a Given Value
-func insertAfter(head *Node, target int, data int) *Node {
-	temp := head
-
-	for temp != nil {
-		if temp.Data == target {
-			newNode := &Node{Data: data}
-
-			newNode.Next = temp.Next
-			temp.Next = newNode
-
-			return head
-		}
-
-		temp = temp.Next
-	}
-
-	return head
-}
-
-// Delete by Value
-func deleteValue(head *Node, value int) *Node {
-
-	if head == nil {
-		return nil
-	}
-
-	// Delete head
-	if head.Data == value {
-		return head.Next
-	}
-
-	temp := head
-
-	for temp.Next != nil && temp.Next.Data != value {
-		temp = temp.Next
-	}
-
-	if temp.Next != nil {
-		temp.Next = temp.Next.Next
-	}
-
-	return head
-}
-
-// Print List
-func printList(head *Node) {
-	temp := head
-
-	for temp != nil {
-		fmt.Print(temp.Data, " -> ")
-		temp = temp.Next
-	}
-
-	fmt.Println("nil")
-}
 
 func main() {
+	str:="hello"
+    m:=make(map[rune]int)
 
-	var head *Node
+	for _,v:=range str{
+     m[v]++
+	}
 
-	// Insert at Head
-	head = insertHead(head, 30)
-	head = insertHead(head, 20)
-	head = insertHead(head, 10)
-
-	fmt.Println("After Insert Head:")
-	printList(head)
-
-	// Insert at Tail
-	head = insertTail(head, 40)
-	head = insertTail(head, 50)
-
-	fmt.Println("After Insert Tail:")
-	printList(head)
-
-	// Insert in Middle
-	head = insertAfter(head, 20, 25)
-
-	fmt.Println("After Insert 25 after 20:")
-	printList(head)
-
-	// Delete Value
-	head = deleteValue(head, 30)
-
-	fmt.Println("After Delete 30:")
-	printList(head)
-}
+	for v,f:=range m{
+		fmt.Println(string(v),f)
+	}
+}     
